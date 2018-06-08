52ac2abc1fc6d5ce263c1e887924f6e98625d1a6
Anh Nguyen <anguyen@swin.edu.au>
2018-06-08 12:48:39 +1000

    Httpclient and Newtonsoft

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Models;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        HttpClient client = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            client.BaseAddress = new Uri("http://jsonplaceholder.typicode.com/");
+
+            HttpResponseMessage httpResponseMessage = await client.GetAsync("posts");
+            
+            var content = await httpResponseMessage.Content.ReadAsStringAsync();
+
+            var posts = JsonConvert.DeserializeObject<List<Post>>(content);
+
+            Post p = new Post { Username = 1, Id = 90000, Body = "Jar Jar Binks", Title = "The shit Star Wars Movie" };
+            Post p2 = new Post { Username = 1, Id = 91000, Body = ": (", Title = "Dragan doesn't know what he's talking about" };
+
+            string json = JsonConvert.SerializeObject(p);
+
+
+        }
+
+
+    }
+}
---
+++ WpfApp1/Models/jsonplaceholder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    class Post
+    {
+        public int Username { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+    class Posts
+    {
+        public List<Post> PostsList { get; set; }
+    }
+}
